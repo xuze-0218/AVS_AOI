dda769e8caf1eb7fcc78e34fee2fc819488ec1e0
泽 徐 <825608904@qq.com>
2026-05-05 17:17:25 +0800

    //

---
+++ AVS_Core/Models/SessionWorkType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVS_Core.Models
+{
+    public enum SessionWorkType
+    {
+        Inspect,      // 检测
+        Calibrate,    // 标定块标定
+        Verify        // 标定块点检
+    }
+}
